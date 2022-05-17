24784579c8b89905c4ddaad502c44e716414d20c
Miro Koczka <usbecko@gmail.com>
2022-05-17 19:29:47 +0200

    api auth

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import "../login/login.scss";
 
 const Login = () => {
-  const hendleSubmit = (e) => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleSubmit = useCallback(async (e) => {
     e.preventDefault();
-  };
+    const response = await fetch("https://fullstack.exercise.applifting.cz/login", {
+      method: "POST",
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json",
+        'X-API-KEY': '6a4fca7d-0167-4ca6-bba8-e8016ab13d71'
+      },
+      body: JSON.stringify({
+        username: email,
+        password
+      })
+    });
+
+    const { access_token } = await response.json();
+
+    if (response.status !== 200) {
+      return;
+    }
+
+    localStorage.setItem("token", access_token);
+    window.location.href = "/";
+  }, [email, password]);
 
   return (
     <div>
-      <form onSubmit={hendleSubmit} className="login-form">
+      <form onSubmit={handleSubmit} className="login-form">
         <h1>Log In</h1>
         <div className="login-input">
           <label>Email</label> <br />
           <input
-            type="email"
             className="form-control"
-            value="Email"
+            value={email}
             placeholder="Your email"
+            onChange={(e) => setEmail(e.target.value)}
             required
           ></input>
         </div>
@@ -26,7 +50,8 @@ const Login = () => {
           <input
             type="password"
             className="form-control"
-            value="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="Your password"
             required
           ></input>
--- src/views/recent/RecentArticles.js
+++ src/views/recent/RecentArticles.js
@@ -6,9 +6,21 @@ const RecentArticles = () => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    axios("https://my-json-server.typicode.com/Naiio97/demo/myArticles")
-      .then((res) => setArticles(res.data))
-      .catch((err) => console.error(err));
+    axios("https://fullstack.exercise.applifting.cz/articles", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": "6a4fca7d-0167-4ca6-bba8-e8016ab13d71",
+        'Authorization': `Bearer ${localStorage.getItem("token")}`
+      }
+    })
+      .then((res) => {
+        setArticles(res.data.items);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return (
